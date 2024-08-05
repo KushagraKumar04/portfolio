a73006072189a5f10fcfd7ec5f12e32c9c397ded
Kushagra Kumar <167355875+KushagraKumar04@users.noreply.github.com>
2024-08-05 11:06:27 +0530

    Update About.tsx

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -7,7 +7,7 @@ import { MoveRight } from 'lucide-react'
 
 export const About = () => {
   return (
-    <SectionContainer id="about" title="Sobre mim">
+    <SectionContainer id="about" title="About Me">
       <div className="flex items-center justify-between md:justify-center">
         <MotionDiv
           initial={{ opacity: 0, x: -24 }}
@@ -17,20 +17,19 @@ export const About = () => {
           className="flex max-w-2xl flex-col gap-6 text-xl md:text-center sm:text-lg"
         >
           <p>
-            Olá, me chamo Daniel Grazziotti. Sou apaixonado por tecnologia e um
-            entusiasta do desenvolvimento web. Tenho utilizado em meus projetos
-            React, Next.js, Node.js, Express e mais.
-          </p>
-
-          <p>
-            Atualmente estudando, praticando e buscando novas oportunidades.
+            Hello, my name is{' '}
+            <span className="font-bold text-target">Kushagra Kumar</span>. I
+            am passionate about technology and a web development enthusiast. I
+            have been using React, Next.js, Node.js, Express, and more in my
+            projects. Currently studying, practicing, and looking for new
+            opportunities.
           </p>
           <MotionDiv className="flex justify-start md:justify-center">
             <Link
               href="#contact"
               className="flex items-center gap-x-2 text-target transition-all hover:gap-x-3 hover:text-[#3385ff]"
             >
-              Contate-me <MoveRight />
+              Contact me <MoveRight />
             </Link>
           </MotionDiv>
         </MotionDiv>

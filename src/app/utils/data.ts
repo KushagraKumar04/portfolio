14607b5a13f0c9e5d30c15bad732a2e969a9b7ec
Kushagra Kumar <167355875+KushagraKumar04@users.noreply.github.com>
2024-08-14 22:29:13 +0530

    Update data.ts

--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -44,7 +44,7 @@ export const projectsData: projectType[] = [
       'https://drive.google.com/uc?export=view&id=19PW6X7bGyfsTvzWcI407HOMlkpN9IKL1',
     description:
       'This project analyzes traffic accident data to identify patterns based on road conditions, weather, and time of day. Visual representations of accident hotspots and contributing factors are created to offer a comprehensive understanding of the dynamics involved. The insights from this analysis aim to develop targeted strategy to improve road safety',
-    Skills: ['python', 'Data Analysis', 'Data Refining', 'pandas', 'Matplot']
+    Skills: ['python', 'Data Analysis', 'Data Refining', 'pandas', 'Matplotlib']
   }
 ]
 
